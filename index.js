79418c7a53e2467b162bf1b23ba5b0b802dd94ef
manveer600 <100722147+manveer600@users.noreply.github.com>
2024-02-10 17:37:38 +0530

    adding on render now

--- index.js
+++ index.js
@@ -9,34 +9,34 @@ dotenv.config();
 connection();
 
 
-// const io = new Server(process.env.SOCKET_PORT, {
-//   // cors: {
-//   //   origin:process.env.FRONTEND_URL,
-//   //   credentials: true,
-//   //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   //   allowedHeaders: ['Content-Type', 'Authorization'],
-//   //   optionsSuccessStatus: 204,
-//   //   allowEIO3: true // Enable CORS for Socket.IO
-//   // },
-// });
+const io = new Server(process.env.SOCKET_PORT, {
+  cors: {
+    origin:process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    optionsSuccessStatus: 204,
+    allowEIO3: true // Enable CORS for Socket.IO
+  },
+});
 
-// // CODE FOR SOCKET.IO IN BACKEND
-// io.on('connection', (socket) => {
-//     socket.on('get-document', async (documentId) => {
-//       const document = await getDocument(documentId);
-//       if(document){
-//         socket.join(documentId);
-//         socket.emit('load-document', document.data);
-//       }
-//       socket.on("send-changes", (delta) => {
-//         socket.broadcast.to(documentId).emit('receive-changes', delta);
-//     })
+// CODE FOR SOCKET.IO IN BACKEND
+io.on('connection', (socket) => {
+    socket.on('get-document', async (documentId) => {
+      const document = await getDocument(documentId);
+      if(document){
+        socket.join(documentId);
+        socket.emit('load-document', document.data);
+      }
+      socket.on("send-changes", (delta) => {
+        socket.broadcast.to(documentId).emit('receive-changes', delta);
+    })
       
-//     socket.on('save-document', async (data) =>{
-//       await updateDocument(documentId,data);
-//     } )
-//   });
-// });
+    socket.on('save-document', async (data) =>{
+      await updateDocument(documentId,data);
+    } )
+  });
+});
 
 
 // const __dirname1 = path.resolve();
